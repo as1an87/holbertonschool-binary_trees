ead76584a9ce9a9a064afdf5ce9993ccccdca4fe
as1an87 <as1anoff87@gmail.com>
2024-05-03 20:33:17 +0000

    updating 11-binary_tree_size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,23 @@
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - counts the number of nodes in a binary tree
+ * @tree: pointer to the root of the binary tree
+ *
+ * Return: number of nodes in the tree or 0 if the tree is empty
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+	return (size + 1);
+}
+
